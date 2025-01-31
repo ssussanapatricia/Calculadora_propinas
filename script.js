7b6a40d33c6944070820cb2171de8b868ec3aaba
Susana <ssussanapatricia@gmail.com>
2025-01-30 18:27:52 -0600

    Calculadora funcionando

--- script.js
+++ script.js
@@ -6,13 +6,38 @@
 // 4. ahora suma el montoCuenta + propina y será totalconPropina
 // 5. imprime el resultado para mostrarlo.
 
-let montoCuenta = parseFloat(prompt("Ingresa el monto total de la cuenta"));
-let porcentajePropina =parseInt(prompt("Ingrese el % de propina que desea pagar"));
-let propina = (montoCuenta * porcentajePropina) / 100;
-let totalconPropina = montoCuenta + propina;
+window.alert("Hola mundooo, esta es la calculadora de propinas");
 
-console.log("La propina es: ");
-console.log("El total de la cuenta con la propina es: ");
+const formulario = document.getElementById("formulario-propina");
+const montoCuentaInput = document.getElementById("montoCuenta");
+const porcentajePropinaInput = document.getElementById("porcentajePropina");
+const propinaSpan = document.getElementById("propina");
+const totalconPropinaSpan = document.getElementById("totalconPropina");
+
+
+document.getElementById("Calcular").addEventListener("click", function () {
+    const montoCuenta = parseFloat(montoCuentaInput.value);
+    const porcentajePropina = parseFloat(porcentajePropinaInput.value);
+
+    if (isNaN(montoCuenta) || isNaN(porcentajePropina)) {
+        alert("Ingresa valores válidos");
+        return;
+    }
+
+    const propina = (montoCuenta*porcentajePropina) / 100;
+    const totalconPropina = montoCuenta + propina;
+
+    propinaSpan.textContent = propina;
+    totalconPropinaSpan.textContent = totalconPropina;
+});
+
+//let montoCuenta = parseFloat(("Ingresa el monto total de la cuenta"));
+//let porcentajePropina =parseInt(("Ingrese el % de propina que desea pagar"));
+//let propina = (montoCuenta * porcentajePropina) / 100;
+//let totalconPropina = montoCuenta + propina;
+
+//console.log("La propina es: ");
+//console.log("El total de la cuenta con la propina es: ");
 
 
 
